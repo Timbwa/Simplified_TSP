90670003272678774c5bfd9c50542f612fc4f98d
Timbwa <davidtimbwa57@gmail.com>
2020-12-06 13:56:16 +0200

    ADD: UCS and BFS. Fixed input bug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import path
+from collections import deque
+from time import time
 import heapq
-
 
 # This implementation is specific for undirected graphs
 # All points in the map will be nodes in my map
@@ -25,21 +26,42 @@ class Node(object):
             self.obstacle = True
 
 
+class Edge(object):
+    def __init__(self, to_node, w):
+        self.to_node = to_node
+        self.weight = w
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f'To Node: {self.to_node.name} Weight: {self.weight}'
+
+    def __eq__(self, other):
+        if isinstance(other, Edge):
+            return self.to_node.name == other.to_node.name  # and self.weight == other.weight
+        return NotImplemented
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+
 # Vertices will be nodes of interest. That is, blanks, and Vertices (' ', 'A-Z')
 class Vertex(Node):
     def __init__(self, name, x_coordinate, y_coordinate, obstacle_flag):
         super(Vertex, self).__init__(name, x_coordinate, y_coordinate, obstacle_flag)
-        self.g_actual_cost = 10000  # the actual cost when using A*, start with high value representing infinity when
+        self.g_actual_cost = 10_000  # the actual cost when using A*, start with high value representing infinity when
         # creating new vertex
         self.h_cost = 0  # the cost given by the heuristic function
         self.previous_vertex = None
         self.total_cost = self.g_actual_cost + self.h_cost
+        self.edges = []
 
     def __str__(self):
         # only print immediate neighbors
         if self.rec_count == 1:
             self.rec_count = 0
-            return "\'" + self.name + "\'"
+            return "\'" + self.name + "\'" + " " + str(self.coordinates)
         else:
             return 'name: ' + "\'" + self.name + "\'" + '\n' + 'coordinates: ' + str(
                 self.coordinates) + '\nObstacle: ' + str(self.obstacle) \
@@ -55,25 +77,15 @@ class Vertex(Node):
     def __lt__(self, other):
         return self.total_cost < other.total_cost
 
-    # def __le__(self, other):
-    # return self.total_cost <= other.total_cost
-
-    # def __ge__(self, other):
-    # return self.total_cost >= other.total_cost
-
     def __eq__(self, other):
         if isinstance(other, Vertex):
-            return self.total_cost == other.total_cost
+            return self.coordinates == other.coordinates
         return NotImplemented
-    #
-    # def __gt__(self, other):
-    #     return self.total_cost > other.total_cost
 
 
 class Graph(object):
-    __vertices = {}  # keep a list of __vertices with their neighbors. Key is vertex coordinates, value is the vertex
-
-    # object
+    def __init__(self):
+        self.__vertices = {}  # keep a list of __vertices with their neighbors.
 
     def add_vertex(self, v):
         # check if the vertex is a Vertex object and if it's not in the __vertices dict
@@ -84,21 +96,46 @@ class Graph(object):
         return False  # if unsuccessful return false
 
     def add_edge(self, v, u):
-        if isinstance(v, Vertex) and isinstance(u,
-                                                Vertex) and v in self.__vertices.values() and u in self.__vertices.values():
+        if isinstance(v, Vertex) and isinstance(u, Vertex) and v in self.__vertices.values() \
+                and u in self.__vertices.values():
             v.neighbors[u.coordinates] = u
             return True
         print("Either vertex may be non-existent in the graph")
         return False
 
+    def add_edge_weight(self, v, u, w):
+        if isinstance(v, Vertex) and isinstance(u, Vertex) and v in self.__vertices.values() \
+                and u in self.__vertices.values():
+            edge = Edge(u, w)
+            edge_1 = Edge(v, w)
+            v.edges.append(edge)
+            u.edges.append(edge_1)
+            return True
+        print("Either vertex may be non-existent in the graph")
+        return False
+
+    def is_empty(self):
+        return not bool(self.__vertices)
+
     def print_graph(self):
-        # v = self.__vertices[(3, 7)]
-        # print(v)
         for c, v in self.__vertices.items():
             print(v)
 
+    def print_graph_edges(self):
+        for c, v in self.__vertices.items():
+            if isinstance(v, Vertex):
+                print(f'{v.name}' + str(v.edges))
+
     def get_vertex(self, coordinates):
         return self.__vertices.get(coordinates)
+
+    # needed for re-evaluation of different paths
+    def reset_values(self):
+        for c, vertex in self.__vertices.items():
+            vertex.g_actual_cost = 10_000
+            vertex.h_cost = 0
+            vertex.total_cost = vertex.g_actual_cost + vertex.h_cost
+            vertex.previous_vertex = None
 
 
 class Map(object):
@@ -133,21 +170,21 @@ def map_to_graph(graph, text_map):
                 vertex = Vertex(el, row_index, col_index, False)
                 graph.add_vertex(vertex)
 
-        # create edges with left-right-down-up order for tie-breaking
+        # create edges with right-left-down-up order for tie-breaking
         for i in range(10):
             for j in range(9):
-                # add right neighbor
-                if j != 8:
-                    graph.add_edge(graph.get_vertex((i, j)), graph.get_vertex((i, j + 1)))
                 # add left neighbor
                 if j != 0:
                     graph.add_edge(graph.get_vertex((i, j)), graph.get_vertex((i, j - 1)))
-                # add down neighbor
-                if i != 9:
-                    graph.add_edge(graph.get_vertex((i, j)), graph.get_vertex((i + 1, j)))
+                # add right neighbor
+                if j != 8:
+                    graph.add_edge(graph.get_vertex((i, j)), graph.get_vertex((i, j + 1)))
                 # add up neighbor
                 if i != 0:
                     graph.add_edge(graph.get_vertex((i, j)), graph.get_vertex((i - 1, j)))
+                # add down neighbor
+                if i != 9:
+                    graph.add_edge(graph.get_vertex((i, j)), graph.get_vertex((i + 1, j)))
 
         return True
     print("Check your parameters !")
@@ -155,15 +192,19 @@ def map_to_graph(graph, text_map):
 
 
 def display_menu():
-    msg = "Welcome, pick an option:"
+    msg = "\nWelcome, pick an option:"
     prompts = {1: "Construct a Shortest Path Graph",
                2: "Solve TSP using BFS and UCS",
                3: "Exit"}
     print(msg)
     for key, value in enumerate(prompts):
         print(str(value) + '. ' + prompts[value])
-    option = int(input("Choice:"))
-    return option
+    try:
+        option = int(input("Choice:"))
+        return option
+    except ValueError as ve:
+        print('Please enter a number from 1-3')
+    return 0
 
 
 def manhattan_heuristic(start_vertex, end_vertex):
@@ -179,13 +220,14 @@ def reconstruct_path(start_vertex, goal_vertex):
         # while a previous vertex exists
         total_path.append(goal_vertex)
         current_vertex = goal_vertex.previous_vertex
-        while not current_vertex.previous_vertex:
+        while current_vertex.previous_vertex:
             total_path.insert(0, current_vertex)
             current_vertex = current_vertex.previous_vertex
+        total_path.insert(0, current_vertex)
         return total_path
 
 
-def solve_a_star(graph, start_vertex, goal_vertex):
+def solve_a_star(graph, start_vertex, goal_vertex, path_lists):
     if isinstance(graph, Graph) and isinstance(start_vertex, Vertex) and isinstance(goal_vertex, Vertex):
         closed_set = []  # visited vertices
         fringe = []
@@ -202,10 +244,12 @@ def solve_a_star(graph, start_vertex, goal_vertex):
             heapq.heapify(fringe)
             current_vertex = heapq.heappop(fringe)
             closed_set.append(current_vertex)
-            print("current vertex: ")
-            print(current_vertex)
+            # print("current vertex: ")
+            # print(current_vertex)
             if isinstance(current_vertex, Vertex):
                 if current_vertex.coordinates == goal_vertex.coordinates:  # if current vertex is goal vertex return
+                    print(f'{start_vertex.name}, {goal_vertex.name}, {goal_vertex.g_actual_cost}')
+                    path_lists.append([start_vertex.name, goal_vertex.name, goal_vertex.g_actual_cost])
                     return goal_vertex.g_actual_cost
 
                 for key, neighbor in current_vertex.neighbors.items():
@@ -228,6 +272,83 @@ def solve_a_star(graph, start_vertex, goal_vertex):
         return "no solution"
 
 
+def solve_tsp_bfs(graph, start_vertex, goal_vertex, path_list):
+    # reset graph values
+    graph.reset_values()
+    # weights in bfs are not considered
+    # arbitrary queue for inserting nodes
+    queue = deque()
+    visited_nodes = []
+    total_cost = 0
+
+    # start with start_vertex. Add edges to queue
+    if isinstance(start_vertex, Vertex) and isinstance(goal_vertex, Vertex):
+        path_list.append(start_vertex.name)
+        for index, edge in enumerate(start_vertex.edges):
+            queue.append(edge)
+        visited_nodes.append(start_vertex)
+
+        # while the queue is not empty
+        while queue:
+            edge = queue.popleft()
+            visited_nodes.append(edge.to_node)
+            path_list.append(edge.to_node.name)
+            if (edge.to_node == goal_vertex and bool(total_cost)) or not queue:
+                # may add weight of goal
+                total_cost += edge.weight + edge.to_node.edges[0].weight
+                path_list.append(start_vertex.name)
+                return total_cost
+            # add all the nodes in the edges in of vertex to queue if it's not visited
+            q_index = 0
+            for index, e in enumerate(edge.to_node.edges):
+                if e not in queue and e.to_node not in visited_nodes:
+                    queue.append(e)
+                    visited_nodes.append(e.to_node)
+                if e in queue:
+                    queue[q_index].weight = e.weight
+                    q_index = q_index + 1
+
+            total_cost += edge.weight
+        return total_cost, path_list
+
+
+def solve_tsp_ucs(graph, start_vertex, goal_vertex, path_list):
+    # reset graph values
+    graph.reset_values()
+    # create min heap for storing edges according to least weight
+    queue = []
+    visited_nodes = []
+    total_cost = 0
+
+    # start with start vertex. Add edges to queue
+    if isinstance(start_vertex, Vertex) and isinstance(goal_vertex, Vertex):
+        path_list.append(start_vertex.name)
+        for index, edge in enumerate(start_vertex.edges):
+            heapq.heappush(queue, edge)
+        heapq.heapify(queue)
+        visited_nodes.append(start_vertex)
+
+        # while the queue isn't empty
+        while queue:
+            edge = heapq.heappop(queue)
+            visited_nodes.append(edge.to_node)
+            path_list.append(edge.to_node.name)
+
+            if (edge.to_node == goal_vertex and bool(total_cost)) or not queue:
+                # may add weight of goal
+                total_cost += edge.weight + edge.to_node.edges[0].weight
+                path_list.append(start_vertex.name)
+                return total_cost
+
+            # add all the neighboring nodes according to their weights
+            queue = []
+            for index, ed in enumerate(edge.to_node.edges):
+                if ed.to_node not in visited_nodes:
+                    heapq.heappush(queue, ed)
+            heapq.heapify(queue)
+            total_cost += edge.weight
+
+
 if __name__ == "__main__":
     # Error messages
     graph_error_msg = "Shortest Path Graph isn't constructed yet...\n"
@@ -241,22 +362,91 @@ if __name__ == "__main__":
 
     # create graph representative of the whole map/puzzle
     map_graph = Graph()
+    map_ucs_graph = Graph()
+
+    # create graph that will contain edge weights
+    tsp_bfs_graph = Graph()
+    tsp_ucs_graph = Graph()
+
+    path_lists = []
+
+    # Vertices A, B, C, D
+    # A is at (8, 2)
+    # B is at (1, 3)
+    # C is at (3, 6)
+    # D is at (8, 6)
+    vertices = [(8, 2), (1, 3), (3, 6), (8, 6)]
 
     # keep displaying menu waiting for input
     while choice != 3:
         if choice == 1:
-            # TO-DO
-            print(map_obj.print_map())
-            # A is at (8, 2)
-            # B is at (1, 3)
-            # C is at (3, 6)
-            # D is at (,)
-            map_to_graph(map_graph, map_obj)
-            cost = solve_a_star(map_graph, map_graph.get_vertex((8, 2)), map_graph.get_vertex((1, 3)))
-            print(f"A-Star cost: {cost}")
+
+            # create fully connected graph from input map
+            if map_graph.is_empty() or map_ucs_graph.is_empty():
+                map_to_graph(map_graph, map_obj)
+                map_to_graph(map_ucs_graph, map_obj)
+
+                # add relevant vertices to graph
+                for v in range(len(vertices)):
+                    temp_vertex = map_graph.get_vertex(vertices[v])
+                    temp_ucs_vertex = map_ucs_graph.get_vertex(vertices[v])
+                    tsp_bfs_graph.add_vertex(temp_vertex)
+                    tsp_ucs_graph.add_vertex(temp_ucs_vertex)
+                # print(tsp_graph.print_graph())
+
+                # for each vertex A, B, C, D find shortest path and add edge
+                for i in range(len(vertices)):
+                    this_vertex = map_graph.get_vertex(vertices[i])
+                    this_ucs_vertex = map_ucs_graph.get_vertex(vertices[i])
+                    temp_list = vertices[i + 1:]
+                    if not temp_list:
+                        break
+                    for j in range(len(temp_list)):
+                        to_vertex = map_graph.get_vertex(temp_list[j])
+                        to_ucs_vertex = map_ucs_graph.get_vertex(temp_list[j])
+                        weight = solve_a_star(map_graph, this_vertex, to_vertex, path_lists)
+                        tsp_bfs_graph.add_edge_weight(this_vertex, to_vertex, weight)
+                        tsp_ucs_graph.add_edge_weight(this_ucs_vertex, to_ucs_vertex, weight)
+                        map_graph.reset_values()
+                        map_ucs_graph.reset_values()
+            else:
+                for path in path_lists:
+                    print(f'{path[0]}, {path[1]}, {path[2]}')
+
         elif choice == 2:
             # TO-DO
-            print("TO-DO")
+            if map_graph.is_empty():
+                print("Error: " + graph_error_msg)
+                choice = display_menu()
+                continue
+            # solve using BFS
+            bfs_path = []
+            bfs_time0 = time()
+            bfs_cost = solve_tsp_bfs(tsp_bfs_graph, tsp_bfs_graph.get_vertex((8, 2)),
+                                     tsp_bfs_graph.get_vertex((8, 2)), bfs_path)
+            bfs_time1 = time()
+            bfs_tot_time = round((bfs_time1 - bfs_time0), 7)
+
+            # solve using UCS
+            ucs_path = []
+            ucs_time0 = time()
+            ucs_cost = solve_tsp_ucs(tsp_ucs_graph, tsp_ucs_graph.get_vertex((8, 2)),
+                                     tsp_ucs_graph.get_vertex((8, 2)), ucs_path)
+            ucs_time1 = time()
+            ucs_tot_time = round((ucs_time1 - ucs_time0), 7)
+            # print results and statistics
+            print(f'\nAlgorithm used: BFS')
+            print(*bfs_path, sep='-')
+            print(f'Total Tour Cost {bfs_cost}\n')
+
+            print(f'\nAlgorithm used: UCS')
+            print(*ucs_path, sep='-')
+            print(f'Total Tour Cost {ucs_cost}\n')
+            print(f'Statistics:\n'
+                  f'\t\t\tNodes\t\t\t\tTime\t\t\tCost\n'
+                  f'BFS\t{bfs_path}\t{bfs_tot_time} sec\t{bfs_cost}\n'
+                  f'UCS\t{ucs_path}\t{ucs_tot_time} sec\t{bfs_cost}')
+
         elif choice == 3:
             print("Bye...")
             break
